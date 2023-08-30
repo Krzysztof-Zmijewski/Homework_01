447236283f01628576c2ca6e6c2aedc7ebb6603a
Krzysztof-Zmijewski <krzysztof.zmijewski00@wp.pl>
2023-08-30 15:46:50 +0200

    Initial commit.

---
+++ src/main/java/pl/coderslab/model/Customer.java
@@ -0,0 +1,17 @@
+package pl.coderslab.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+    private int id;
+    private String firstname;
+    private String lastname;
+}
